e5c4e4058aad5cafa7d53e66e13cd654b3e472fd
jpenariasholberton <1875@holbertonschool.com>
2020-08-20 00:18:58 +0000

    fix betty

--- hsh.c
+++ hsh.c
@@ -9,54 +9,44 @@
 
 int hsh(char **av, char **env)
 {
-        char *line = NULL, *path, *commandPath;
-        size_t len;
-        char **commandTokens;
-        int read = 0, status_builtins, mode = 1;
+	char *line = NULL, *path, *commandPath;
+	size_t len;
+	char **commandTokens;
+	int read = 0, status_builtins, mode = 1;
 
-        (void) av;
-        (void) env;
+	(void)av;
+	(void)env;
 
-        isatty(STDIN_FILENO) == 0 ? mode = 0 : mode;
-        while (1)
-        {
-                mode == 1 ? write(STDIN_FILENO, "$ ", 2) : mode;
-                read = getline(&line, &len, stdin);
-                if (read == EOF)
-                {
-                        free(line), write(STDIN_FILENO, "\n", 1);
-                        return (0);
-                }
-                else
-                {
-                        if (read > 1)
-                        {
-                                commandTokens = tokenizer(line);
-                                status_builtins = builtins(commandTokens[0]);
-                                /*if (status_builtins == -1 || status_builtins == 0)
-                                  {
-				  *free(line);*
-				  *free(commandTokens);*
-				  }*/
-                                if (status_builtins == -1)
-                                        exit(1);
-                                if (status_builtins == 0)
-                                        continue;
-                                path = _getenv();
-                                if (path == NULL)
-                                        commandPath = commandTokens[0];
-                                else
-                                        commandPath = _which(path, commandTokens[0]);
+	isatty(STDIN_FILENO) == 0 ? mode = 0 : mode;
+	while (1)
+	{
+		mode == 1 ? write(STDIN_FILENO, "$ ", 2) : mode;
+		read = getline(&line, &len, stdin);
+		if (read == EOF)
+		{
+			free(line), write(STDIN_FILENO, "\n", 1);
+			return (0);
+		}
+		else
+		{
+			if (read > 1)
+			{
+				commandTokens = tokenizer(line);
+				status_builtins = builtins(commandTokens[0]);
+				if (status_builtins == -1)
+					exit(1);
+				if (status_builtins == 0)
+					continue;
+				path = _getenv();
+				if (path == NULL)
+					commandPath = commandTokens[0];
+				else
+					commandPath = _which(path, commandTokens[0]);
 
-                                forking(commandPath, commandTokens);
-
-                                /*for(idx= 0; idx < 2; idx++)
-                                  free(commandTokens[idx]);*/
-                                /*free(line);*/
-                                free(commandTokens);
-                                free(path);
-                                /*free(commandPath);*/
-                        }
-                }
-        }
+				forking(commandPath, commandTokens);
+				free(commandTokens);
+				free(path);
+			}
+		}
+	}
 }
